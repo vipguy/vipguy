89797fb12f80ad42f4a70b32988c910a29f4172f
vipguy <74574086+vipguy@users.noreply.github.com>
2023-04-26 00:05:30 -0400

    Update kikbotBattery.py

--- kikbotBattery.py
+++ kikbotBattery.py
@@ -2,9 +2,10 @@
 #import psutil
 
 
-if ("battery"== chat_message.body.lower()):
-            battery_detecting = psutil.sensors_battery()
-            plugged = battery_detecting.power_plugged
-            percent_battery = str(battery_detecting.percent)
-            plugged = "Plugged In" if plugged else "Not Plugged In"
-            self.client.send_chat_message(chat_message.group_jid,f"{(percent_battery+'% | '+plugged)}")
+if chat_message.body.strip().lower() == "battery":
+            battery = psutil.sensors_battery()
+            percent = battery.percent
+            power_plugged = battery.power_plugged
+            plugged = "Plugged In" if power_plugged else "Not Plugged In"
+            message = f"Battery level: {percent}% ({plugged})"
+            self.client.send_chat_message(chat_message.group_jid, message)
