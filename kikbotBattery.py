7624e80992cb401d57f25cfd8015db087c057ec9
vipguy <74574086+vipguy@users.noreply.github.com>
2023-04-30 22:44:18 -0400

    Update kikbotBattery.py
    import psutil
    import matplotlib
    matplotlib.use('Agg')
    import matplotlib.pyplot as plt
    from io import BytesIO
    
    updates

--- kikbotBattery.py
+++ kikbotBattery.py
@@ -1,5 +1,8 @@
-#LOCAL BOT BATTERY LVL #
-#import psutil
+import psutil
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from io import BytesIO
 
 
 if chat_message.body.strip().lower() == "battery":
@@ -8,4 +11,18 @@ if chat_message.body.strip().lower() == "battery":
             power_plugged = battery.power_plugged
             plugged = "Plugged In" if power_plugged else "Not Plugged In"
             message = f"Battery level: {percent}% ({plugged})"
+    
+            # Create a bar chart of the battery level
+            fig, ax = plt.subplots()
+            ax.bar(['Battery'], [percent])
+            ax.set_ylim(0, 100)
+            ax.set_ylabel('Battery Level (%)')
+            ax.set_title('Battery Status')
+            buffer = BytesIO()
+            plt.savefig(buffer, format='png')
+    
+            # Send the message and image to the group chat
+            buffer.seek(0)
+            image_data = buffer.getvalue()
             self.client.send_chat_message(chat_message.group_jid, message)
+            self.client.send_chat_image(chat_message.group_jid, image_data)
