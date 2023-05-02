cb0138f6cf235d309778b64045670e17aa19c893
vipguy <74574086+vipguy@users.noreply.github.com>
2023-05-02 01:26:37 -0400

    Update kikbotBattery.py

--- kikbotBattery.py
+++ kikbotBattery.py
@@ -5,24 +5,24 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 
 
-if chat_message.body.strip().lower() == "battery":
-            battery = psutil.sensors_battery()
-            percent = battery.percent
-            power_plugged = battery.power_plugged
-            plugged = "Plugged In" if power_plugged else "Not Plugged In"
-            message = f"Battery level: {percent}% ({plugged})"
+if chat_message.body.strip().lower() == "battery":  # Check if the chat message is asking for battery info
+            battery = psutil.sensors_battery()  # Get the battery information using the psutil library
+            percent = battery.percent  # Get the battery percentage
+            power_plugged = battery.power_plugged  # Check if the power is plugged in or not
+            plugged = "Plugged In" if power_plugged else "Not Plugged In"  # Set the "plugged" variable based on the power plugged status
+            message = f"Battery level: {percent}% ({plugged})"  # Create a message with the battery percentage and power status
     
-            # Create a bar chart of the battery level
+            # Create a bar chart of the battery level using matplotlib
             fig, ax = plt.subplots()
             ax.bar(['Battery'], [percent])
             ax.set_ylim(0, 100)
             ax.set_ylabel('Battery Level (%)')
             ax.set_title('Battery Status')
             buffer = BytesIO()
-            plt.savefig(buffer, format='png')
+            plt.savefig(buffer, format='png')  # Save the bar chart as a PNG image in a BytesIO buffer
     
             # Send the message and image to the group chat
             buffer.seek(0)
             image_data = buffer.getvalue()
-            self.client.send_chat_message(chat_message.group_jid, message)
-            self.client.send_chat_image(chat_message.group_jid, image_data)
+            self.client.send_chat_message(chat_message.group_jid, message)  # Send the battery info message to the group chat
+            self.client.send_chat_image(chat_message.group_jid, image_data)  # Send the bar chart image to the group chat
